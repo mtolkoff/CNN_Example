45b7c2a28a31c8500b1b0e01898578d14f19e6b0
Max Tolkoff <mtolkoff@gmail.com>
2019-05-07 19:20:53 -0400

    fix indentation error

--- Read_CNN.py
+++ Read_CNN.py
@@ -151,21 +151,21 @@ if __name__ == '__main__':
             100 * correct / total))
 
     # What classes perform well?
-    if __name__ == '__main__':
-        class_correct = list(0. for i in range(len(classes)))
-        class_total = list(0. for i in range(len(classes)))
-        with torch.no_grad():
-            for data in testloader:
-                images, labels = data
-                outputs = net(images)
-                _, predicted = torch.max(outputs, 1)
-                if len(labels) != 1:
-                    c = (predicted == labels).squeeze()
-                for i in range(len(labels)):
-                    label = labels[i]
-                    class_correct[label] += c[i].item()
-                    class_total[label] += 1
+if __name__ == '__main__':
+    class_correct = list(0. for i in range(len(classes)))
+    class_total = list(0. for i in range(len(classes)))
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            if len(labels) != 1:
+                c = (predicted == labels).squeeze()
+            for i in range(len(labels)):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
 
-        for i in range(len(classes)):
-            print('Accuracy of %5s : %2d %%' % (
-                classes[i], 100 * class_correct[i] / class_total[i]))
+    for i in range(len(classes)):
+        print('Accuracy of %5s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
