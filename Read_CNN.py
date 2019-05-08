ac50e593607a97eb14c593d880fd8946f78a8507
Max Tolkoff <mtolkoff@gmail.com>
2019-05-08 00:48:16 -0400

    fixes bugs causing model to not save correctly

--- Read_CNN.py
+++ Read_CNN.py
@@ -20,23 +20,24 @@ import numpy as np
 #         tuple_with_path = (original_tuple + (path,))
 #         return tuple_with_path
 
+if __name__ == '__main__':
+    torch.manual_seed(123)
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-trainset = datasets.ImageFolder(root='Data/intel/seg_train', transform=transform)
+    trainset = datasets.ImageFolder(root='Data/intel/seg_train', transform=transform)
 # trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
 #                                        download=True, transform=transform)
-trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+    trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
 
-testset = datasets.ImageFolder(root='Data/intel/seg_test', transform=transform)
+    testset = datasets.ImageFolder(root='Data/intel/seg_test', transform=transform)
 # testset = torchvision.datasets.CIFAR10(root='./data', train=False,
 #                                       download=True, transform=transform)
-testloader = DataLoader(testset, batch_size=4, shuffle=True, num_workers=2)
+    testloader = DataLoader(testset, batch_size=4, shuffle=True, num_workers=2)
 
-classes = os.listdir('Data/intel/seg_test')
+    classes = os.listdir('Data/intel/seg_test')
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # classes = ('plane', 'car', 'bird', 'cat',
 #           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
@@ -47,11 +48,11 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #         if inputs.shape[2] != 150 or inputs.shape[3] != 150:
 #             print(path)
 # functions to show an image
-def imshow(img):
-    img = img / 2 + 0.5  # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+    def imshow(img):
+        img = img / 2 + 0.5  # unnormalize
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        plt.show()
 
 
 # get some random training images
@@ -64,32 +65,32 @@ def imshow(img):
 # print labels
 #    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 8, stride=2)
-        self.pool = nn.MaxPool2d(3, 3)
-        self.conv2 = nn.Conv2d(6, 16, 8, stride=2)
-        self.fc1 = nn.Linear(144, 120)
-        self.fc2 = []
-        for i in range(3):
-            self.fc2.append(nn.Linear(120, 120))
-        self.fc3 = nn.Linear(120, 84)
-        self.fc4 = nn.Linear(84, len(classes))
+    class Net(nn.Module):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.conv1 = nn.Conv2d(3, 6, 8, stride=2)
+            self.pool = nn.MaxPool2d(3, 3)
+            self.conv2 = nn.Conv2d(6, 16, 8, stride=2)
+            self.fc1 = nn.Linear(144, 120)
+            self.fc2 = []
+            for i in range(3):
+                self.fc2.append(nn.Linear(120, 120))
+            self.fc3 = nn.Linear(120, 84)
+            self.fc4 = nn.Linear(84, len(classes))
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 144)
-        x = F.relu(self.fc1(x))
-        for i in range(3):
-            x = torch.tanh(self.fc2[i](x))
-        x = F.relu(self.fc3(x))
-        x = self.fc4(x)
-        return x
+        def forward(self, x):
+            x = self.pool(F.relu(self.conv1(x)))
+            x = self.pool(F.relu(self.conv2(x)))
+            x = x.view(-1, 144)
+            x = F.relu(self.fc1(x))
+            for i in range(3):
+                x = torch.tanh(self.fc2[i](x))
+            x = F.relu(self.fc3(x))
+            x = self.fc4(x)
+            return x
 
 
-net = Net()
+    net = Net()
 # net = torchvision.models.resnet18(pretrained=True)
 # for param in net.parameters():
 #     param.requires_grad = False
@@ -104,12 +105,11 @@ net = Net()
 #                        )
 
 #net.to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-if __name__ == '__main__':
+
     for epoch in range(1):  # loop over the dataset multiple times
-
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
             # get the inputs
@@ -134,9 +134,8 @@ if __name__ == '__main__':
 
     print('Finished Training')
 
-torch.save(net.state_dict(), "transfer-4-tanh.pt")
+    torch.save(net.state_dict(), "transfer-4-tanh.pt")
 
-if __name__ == '__main__':
     correct = 0
     total = 0
     with torch.no_grad():
@@ -151,7 +150,6 @@ if __name__ == '__main__':
             100 * correct / total))
 
     # What classes perform well?
-if __name__ == '__main__':
     class_correct = list(0. for i in range(len(classes)))
     class_total = list(0. for i in range(len(classes)))
     with torch.no_grad():
